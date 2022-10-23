fb15077990b2e27446ebde7cc9c02bdcf132aff8
smciwa <smciwa@student.wethinkcode.co.za>
2022-10-23 13:21:21 +0200

    removed unused file

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return (MaterialApp(
-      title: 'Select Year',
+      title: 'F1 project',
       home: Scaffold(
         appBar: AppBar(
           title: const Center(
